f9387396186d4474128e067833c6a0686277fb96
stop-sine <57299283+stop-sine@users.noreply.github.com>
2025-09-25 23:09:31 -0500

    Fix namespace declaration and organize using directives

--- ConversationsRaiseSpeechcraft/Program.cs
+++ ConversationsRaiseSpeechcraft/Program.cs
@@ -1,8 +1,18 @@
 using Mutagen.Bethesda;
 using Mutagen.Bethesda.Synthesis;
 using Mutagen.Bethesda.Skyrim;
+using DynamicData;
+using static Mutagen.Bethesda.Skyrim.Condition;
+using Noggog;
+using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Plugins.Records;
+using System.Data;
+using Mutagen.Bethesda.FormKeys.SkyrimSE;
+using CommandLine;
+using Mutagen.Bethesda.Plugins.Binary.Headers;
+using Microsoft.VisualBasic.FileIO;
 
-namespace Conversations-Raise-Speechcraft
+namespace ConversationsRaiseSpeechcraft
 {
     public class Program
     {
