365dcaee6aef4186d39b3eff51e82d973b356956
stop-sine <57299283+stop-sine@users.noreply.github.com>
2025-09-26 10:46:52 -0500

    Implement dialog response patching and message construction

--- ConversationsRaiseSpeechcraft/Program.cs
+++ ConversationsRaiseSpeechcraft/Program.cs
@@ -16,17 +16,136 @@ namespace ConversationsRaiseSpeechcraft
 {
     public class Program
     {
+
         public static async Task<int> Main(string[] args)
         {
             return await SynthesisPipeline.Instance
                 .AddPatch<ISkyrimMod, ISkyrimModGetter>(RunPatch)
-                .SetTypicalOpen(GameRelease.SkyrimSE, "YourPatcher.esp")
+                .SetTypicalOpen(GameRelease.SkyrimSE, "Conversations Raise Speechcraft.esp")
                 .Run(args);
+        }
+
+        private static List<IDialogResponsesGetter> CollectGroups(IEnumerable<IDialogTopicGetter> dialCollection)
+        {
+            return [.. dialCollection
+                .Reverse()
+                .SelectMany(dial => dial.Responses)
+                .GroupBy(info => info.FormKey)
+                .Select(g => g.Last())];
+        }
+
+        private static void PackageInfoOverrides(ref DialogTopic dial, Dictionary<FormKey, List<IDialogResponsesGetter>> groups)
+        {
+            var groupGetter = groups[dial.FormKey];
+            dial.Responses.Clear();
+            dial.Responses.Add(groupGetter.Select(i => i.DeepCopy()));
+        }
+
+        private static IFormLinkGetter<IMessageGetter> ConstructMessage(ISkyrimMod patchMod)
+        {
+            var mesg = new Message(patchMod)
+            {
+                EditorID = "ANDR_CRS_EXPGainedMessage",
+                Description = "Your skill in Speech has increased.",
+                DisplayTime = 2
+            };
+            return mesg.ToLink<IMessageGetter>();
+        }
+
+        private static IFormLinkGetter<IQuestGetter> ConstructQuest(ISkyrimMod patchMod, int dialCount)
+        {
+            var qust = new Quest(patchMod)
+            {
+                EditorID = "ANDR_CRS_Quest",
+                Name = "ANDR_CRS_Quest",
+                VirtualMachineAdapter = new QuestAdapter()
+                {
+                    Scripts = [new ScriptEntry {
+                        Name = "ANDR_CRS_QuestScript",
+                        Flags = 0,
+                        Properties = [new ScriptBoolListProperty{
+                            Name = "ConversationBool",
+                            Flags = ScriptProperty.Flag.Edited,
+                            Data = [.. new bool[dialCount]]
+                        }]
+                    }]
+                },
+                Flags = Quest.Flag.StartGameEnabled,
+                Priority = 0,
+                Type = Quest.TypeEnum.Misc,
+                NextAliasID = 0
+            };
+            return qust.ToLink<IQuestGetter>();
+        }
+
+        private static IFormLinkGetter<IGlobalGetter> ConstructGlobal(ISkyrimMod patchMod)
+        {
+            var glob = new GlobalShort(patchMod)
+            {
+                EditorID = "ANDR_CRS_EXPGainGlobal_Medium",
+                Data = 50
+            };
+            return glob.ToLink<IGlobalGetter>();
+        }
+
+
+        private static void PatchInfo(DialogResponses info, IFormLink<IMessageGetter> mesg, IFormLink<IQuestGetter> qust, IFormLink<IGlobalGetter> glob)
+        {
+            info.VirtualMachineAdapter?.Scripts.Add(new ScriptEntry
+            {
+                Name = "ANDR_CRS_DialogueXPScript",
+                Flags = 0,
+                Properties = [new ScriptObjectProperty {
+                    Name = "ANDR_CRS_EXPGainedMessage",
+                    Flags = ScriptProperty.Flag.Edited,
+                    Object = mesg
+                }, new ScriptIntProperty {
+                    Name = "ANDR_CRS_Index",
+                    Flags = ScriptProperty.Flag.Edited,
+                    Data = 2
+                }, new ScriptObjectProperty {
+                    Name = "ANDR_CRS_Quest",
+                    Flags = ScriptProperty.Flag.Edited,
+                    Object = qust
+                }, new ScriptObjectProperty {
+                    Name = "EXPGainGlobal",
+                    Flags = ScriptProperty.Flag.Edited,
+                    Object = glob
+                }]
+            });
         }
 
         public static void RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
         {
-            //Your code here!
+            var cache = state.LinkCache;
+            var patchMod = state.PatchMod;
+            var dialogTopicRecords = state.LoadOrder.PriorityOrder.DialogTopic().WinningOverrides().ToList();
+            var groupRecords = dialogTopicRecords.ToDictionary(
+                d => d.FormKey,
+                d => CollectGroups(d.FormKey.ToLinkGetter<IDialogTopicGetter>().ResolveAll(cache)));
+
+            var modkeys = dialogTopicRecords.Select(r => r.FormKey.ModKey);
+            Console.WriteLine($"Found {dialogTopicRecords.Count} DIAL records in {modkeys.Distinct().Count()} plugins");
+            var subrecordCount = 0;
+            foreach (var group in groupRecords.Values)
+                subrecordCount += group.Count;
+            Console.WriteLine($"Found {subrecordCount} INFO subrecords to be patched");
+
+
+
+
+
+
+
+            foreach (var record in dialogTopicRecords)
+            {
+                Console.WriteLine($"Patching {record.FormKey}");
+                var dial = patchMod.DialogTopics.GetOrAddAsOverride(record);
+                PackageInfoOverrides(ref dial, groupRecords);
+                var grup = dial.Responses;
+            }
+
+
         }
     }
 }
